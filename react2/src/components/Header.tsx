f87569573f01719584907f36f5e7bb35c4a3cce3
merci2 <karganja@yahoo.de>
2025-09-11 15:11:41 +0200

    Update Header.tsx

--- react2/src/components/Header.tsx
+++ react2/src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { useAuth } from '../config/useAuth';
-import { appConfig } from '../config/authConfig';
+// import { appConfig } from '../config/authConfig';
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
